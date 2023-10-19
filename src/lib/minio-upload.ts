912ae509f3e54460d4dad5108f9d8acf2e302437
fabioportieri <fazfazzi@gmail.com>
2023-10-19 12:31:54 +0200

    fix docker compose rete chatpdf_net

--- src/lib/minio-upload.ts
+++ src/lib/minio-upload.ts
@@ -1,17 +1,13 @@
-
-import { FILE_KEY_SEPARATOR } from "./utils";
-import * as minio from "minio";
 import { MINIO_BUCKET_NAME, NodeFile, minioClient } from "./minio";
+import { FILE_KEY_SEPARATOR } from "./utils";
 
 // https://github.com/minio/minio-js
 // https://min.io/docs/minio/linux/developers/javascript/API.html
-
 
 export async function uploadToMinioServer(
   file: NodeFile | File
 ): Promise<{ file_key: string; file_name: string }> {
   try {
-
     const file_key =
       "uploads/" +
       Date.now().toString() +
@@ -20,25 +16,30 @@ export async function uploadToMinioServer(
 
     let bodyFile: Buffer = Buffer.from(await file.arrayBuffer());
 
-    console.log("🚀 ~ uploadToMINIO ~ upload file_key:", file_key, "buffer length:", bodyFile.length);
+    console.log(
+      "🚀 ~ uploadToMINIO ~ upload file_key:",
+      file_key,
+      "buffer length:",
+      bodyFile.length
+    );
 
     // https://min.io/docs/minio/linux/developers/javascript/API.html#putObject
     // putObject(bucketName: string, objectName: string, stream: ReadableStream | Buffer | string, metaData?: ItemBucketMetadata): Promise<UploadedObjectInfo>;
     // fPutObject(bucketName: string, objectName: string, filePath: string, metaData?: ItemBucketMetadata): Promise<UploadedObjectInfo>;
 
-    const client = new minio.Client({
-      endPoint: 'localhost',
-      port: 9040,
-      useSSL: false,
-      accessKey: 'pDLNypgWK4n4IidH',
-      secretKey: '6pvgqJI9Bhxn0bEYp5NEk8xS8wDHW4ox',
-    })
+    // const client = new minio.Client({
+    //   endPoint: 'localhost',
+    //   port: 9040,
+    //   useSSL: false,
+    //   accessKey: 'pDLNypgWK4n4IidH',
+    //   secretKey: '6pvgqJI9Bhxn0bEYp5NEk8xS8wDHW4ox',
+    // })
 
-
-
-    const data = await client.putObject(MINIO_BUCKET_NAME, file_key, bodyFile);
-
-
+    const data = await minioClient.putObject(
+      MINIO_BUCKET_NAME,
+      file_key,
+      bodyFile
+    );
 
     console.log("🚀 ~ uploadToMINIO ~ data:", data);
     return {
@@ -73,4 +74,3 @@ object-uploader.mjs:23 Uncaught (in promise) TypeError: Class extends value unde
     at eval (webpack-internal:///(:3002/app-pages-browser)/./src/lib/minio-upload.ts:6:64)
 
 */
-
